492a94810816b65402480287d389c7b062f3b9a6
vicziani <viczian.istvan@gmail.com>
2024-05-22 16:44:45 +0200

    REST API rendbehozatal

--- employees-web/src/main/java/employees/EmployeeController.java
+++ employees-web/src/main/java/employees/EmployeeController.java
@@ -2,9 +2,13 @@ package employees;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.net.URI;
 
 @RestController
 @AllArgsConstructor
@@ -19,27 +23,34 @@ public class EmployeeController {
     }
 
     @GetMapping("/{id}")
-    public Mono<EmployeeDto> findById(@PathVariable long id) {
-        return employeeService.findById(id);
+    public Mono<ResponseEntity<EmployeeDto>> findById(@PathVariable long id) {
+        return employeeService.findById(id)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public Mono<EmployeeDto> create(@Valid @RequestBody EmployeeDto employeeDto) {
+    public Mono<ResponseEntity<EmployeeDto>> create(@Valid @RequestBody EmployeeDto employeeDto) {
         if (employeeDto.id() != null) {
             throw new IllegalArgumentException("Do not send id!");
         }
-        return employeeService.save(employeeDto);
+        return employeeService
+                .save(employeeDto)
+                .map(result -> ResponseEntity.created(URI.create("/api/employees/%d".formatted(result.id()))).body(result));
     }
 
     @PutMapping("/{id}")
-    public Mono<EmployeeDto> update(@PathVariable long id, @Valid @RequestBody EmployeeDto employeeDto) {
+    public Mono<ResponseEntity<EmployeeDto>> update(@PathVariable long id, @Valid @RequestBody EmployeeDto employeeDto) {
         if (id != employeeDto.id()) {
             throw new IllegalArgumentException("Ids are not the same");
         }
-        return employeeService.save(employeeDto);
+        return employeeService.save(employeeDto)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public Mono<Void> deleteById(@PathVariable long id) {
         return employeeService.deleteById(id);
     }
