1b16031d6641a9a77433471956406090123e7cdf
vicziani <viczian.istvan@gmail.com>
2024-05-23 13:49:34 +0200

    JSON deszerializáció

--- employees-restclient/src/main/java/employees/ClientRunner.java
+++ employees-restclient/src/main/java/employees/ClientRunner.java
@@ -1,22 +1,30 @@
 package employees;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
+@AllArgsConstructor
+@Slf4j
 public class ClientRunner implements CommandLineRunner  {
 
-    @Autowired
     private WebClient webClient;
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("hello");
+        log.info("hello");
 
         var json = webClient.get().uri("/api/employees").exchangeToMono(response -> response.bodyToMono(String.class)).block();
-        System.out.println(json);
+        log.info(json);
+
+        var employees = webClient.get().uri("/api/employees").exchangeToFlux(clientResponse -> clientResponse.bodyToFlux(Employee.class)).collectList().block();
+        employees.forEach(employee -> log.info("employee: {}", employee));
     }
+
+    public record Employee(Long id, String name) {}
 
 }
